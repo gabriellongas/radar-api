b0629b3d1decb23e45f82b96d83f33a6afe689a1
Lucas Pires Savassa <lucaspsavassa@gmail.com>
2023-10-24 15:53:16 -0300

    Add basic api server using Entity Framework

---
+++ Radar.Api/Controllers/PessoasController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Radar.Api.Models;
+
+namespace Radar.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoasController : ControllerBase
+    {
+        private readonly RadarContext _context;
+
+        public PessoasController(RadarContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pessoa>>> GetPessoa()
+        {
+            if (_context.Pessoa == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Pessoa.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pessoa>> GetPessoa(int id)
+        {
+            if (_context.Pessoa == null)
+            {
+                return NotFound();
+            }
+
+            var pessoa = await _context.Pessoa.FindAsync(id);
+
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            return pessoa;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPessoa(int id, Pessoa pessoa)
+        {
+            if (id != pessoa.PessoaID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pessoa).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PessoaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Pessoa>> PostPessoa(Pessoa pessoa)
+        {
+            if (_context.Pessoa == null)
+            {
+                return Problem("Entity set 'RadarContext.Pessoa'  is null.");
+            }
+
+            _context.Pessoa.Add(pessoa);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPessoa", new { id = pessoa.PessoaID }, pessoa);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePessoa(int id)
+        {
+            if (_context.Pessoa == null)
+            {
+                return NotFound();
+            }
+            
+            var pessoa = await _context.Pessoa.FindAsync(id);
+            
+            if (pessoa == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pessoa.Remove(pessoa);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PessoaExists(int id)
+        {
+            return (_context.Pessoa?.Any(e => e.PessoaID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Radar.Api/Models/RadarContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Radar.Api.Models;
+
+namespace Radar.Api.Models
+{
+    public class RadarContext : DbContext
+    {
+        public RadarContext(DbContextOptions<RadarContext> options) : base(options) { }
+
+        public DbSet<Post> Post { get; set; }
+
+        public DbSet<Pessoa> Pessoa { get; set; }
+
+        public DbSet<Local> Local { get; set; }
+    }
+}
